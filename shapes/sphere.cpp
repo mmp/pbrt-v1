mmp/pbrt-v1

commit 52658c2a7cdd639f86aaf19563e466566f2c1b5c

Bug fix due to Xiong "Tomwindcloud" Wei: smarter way of handling rays that just miss the sphere.

shapes/sphere.cpp
@@ -60,11 +60,8 @@ public:
 		Point ps;
 		Ray r(p,
 		      UniformSampleCone(u1, u2, cosThetaMax, wcX, wcY, wc));
-		if (!Intersect(r, &thit, &dgSphere)) {
-			ps = Pcenter - radius * wc;
-		} else {
-			ps = r(thit);
-		}
+		if (!Intersect(r, &thit, &dgSphere))
+			thit = Dot(Pcenter - p, Normalize(r.d));
 		*ns = Normal(Normalize(ps - Pcenter));
 		if (reverseOrientation) *ns *= -1.f;
 		return ps;

commit e7905848c25ba9d8b0fc430f5544e77a51a8d809

Fix bug introduced in commit 52658c2a7cdd639f86aaf19563e466566f2c1b5c: sampled point on sphere was no longer being computed!

shapes/sphere.cpp
@@ -62,6 +62,7 @@ public:
 		      UniformSampleCone(u1, u2, cosThetaMax, wcX, wcY, wc));
 		if (!Intersect(r, &thit, &dgSphere))
 			thit = Dot(Pcenter - p, Normalize(r.d));
+		ps = r(thit);
 		*ns = Normal(Normalize(ps - Pcenter));
 		if (reverseOrientation) *ns *= -1.f;
 		return ps;